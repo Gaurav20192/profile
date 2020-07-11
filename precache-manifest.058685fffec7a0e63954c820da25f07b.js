0469209a9cf710e47f8b9241a62b4b22515d5aea
Gaurav <gaurav.k20192@gmail.com>
2020-07-11 19:30:29 +0530

    Updates

---
+++ precache-manifest.058685fffec7a0e63954c820da25f07b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f413bcd082eb53265d20b1a424ce881",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "22edd99ef7444b6c8e26",
+    "url": "/profile/static/css/main.a5117af6.chunk.css"
+  },
+  {
+    "revision": "a96dd41a0e291720709d",
+    "url": "/profile/static/js/2.2af2938d.chunk.js"
+  },
+  {
+    "revision": "ad9d1009afdbe548e9d52b63c0ed6066",
+    "url": "/profile/static/js/2.2af2938d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22edd99ef7444b6c8e26",
+    "url": "/profile/static/js/main.035d538a.chunk.js"
+  },
+  {
+    "revision": "e07b11cf2cbd882e8112",
+    "url": "/profile/static/js/runtime-main.04aed289.js"
+  },
+  {
+    "revision": "8e51f3bbbc7c7d84a6278bb8786f4573",
+    "url": "/profile/static/media/Capture.8e51f3bb.gif"
+  },
+  {
+    "revision": "152888365b406bdd5349626369fcb603",
+    "url": "/profile/static/media/man.15288836.svg"
+  },
+  {
+    "revision": "797799c08f35f2b13c07b2d56f50feef",
+    "url": "/profile/static/media/man_laptop.797799c0.svg"
+  },
+  {
+    "revision": "0ef2353e80c71c450941585bf56a6241",
+    "url": "/profile/static/media/skills.0ef2353e.svg"
+  }
+]);
